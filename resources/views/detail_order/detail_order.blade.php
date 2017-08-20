f4a98fdd77bfd22bbdcb6ca90da69d8c601d1e41
Aisy Rozsidhy <aisymuhammad@gmail.com>
2017-08-20 18:18:07 +0700

    fix detail order

--- resources/views/detail_order/detail_order.blade.php
+++ resources/views/detail_order/detail_order.blade.php
@@ -3,8 +3,11 @@
 @section('link_nav')
 <li><a href="{{ url('home') }}"><i class="fa fa-home"></i> Halaman Utama</a></li>
 <li><a href="{{ url('order') }}"><i class="fa fa-inbox"></i> Order</a></li>
+<li><a href="{{ url('restoran') }}"><i class="fa fa-cutlery"></i> Restoran</a></li>
+<li><a href="{{ url('tarif') }}"><i class="fa fa-money"></i> Tarif</a></li>
 <li><a href="{{ url('kurir') }}"><i class="fa fa-motorcycle"></i> Kurir</a></li>
 <li><a href="{{ url('admin') }}"><i class="fa fa-users"></i> Admin</a></li>
+<li><a href="{{ url('pengguna') }}"><i class="fa fa-users"></i> Pengguna</a></li>
 @stop
 
 @section('title', 'Detail Order')
@@ -23,13 +26,15 @@
 								<th>id</th>
 								<th>id order</th>
 								<th>nama order</th>
-								<th>keterangan</th>
-								<th>alamat</th>
+								<th>alamat restoran</th>
 								<th>jumlah</th>
 								<th>harga</th>
+								<th>total harga</th>
 							</tr>
 						</thead>
 						<tbody>
+							
+							{{ $total=0 }}
 
 							@foreach ($data_detail as $element)
 							<tr>
@@ -48,13 +53,19 @@
 									@endif
 								</td>
 								<td>{{ $element['menu_id'] }}</td>
-								<td>{{ $element['keterangan'] }}</td>
 								<td>{{ $element['alamat'] }}</td>
 								<td>{{ $element['jumlah'] }}</td>
 								<td>{{ $element['harga'] }}</td>
-								<td></td>
+								<td>{{ $harga_total = $element['jumlah']*$element['harga'] }}</td>
 							</tr>
+							{{ $total += $harga_total }}
 							@endforeach
+							<tr>
+								<td colspan="6" class="grey">
+									<b>Total yang dibayar</b>
+								</td>
+								<td>Rp. {{ $total }}</td>
+							</tr>
 
 						</tbody>
 					</table>
